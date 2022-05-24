2f4b3afa44c19f6f5ff53409484c7b11a31efc95
DilonAndriesse <70940653+DilonAndriesse@users.noreply.github.com>
2022-05-24 10:31:37 +0200

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -14,7 +14,7 @@ def connect():
         host="mysql.dehoogjes.nl",
         user="dehoogjesnl",
         db="dehoogjesnl",
-        password="HoogKlooster19",
+        password="**************",
         auth_plugin="mysql_native_password")
 
     # Open a cursor.
